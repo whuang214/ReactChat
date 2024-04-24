cd313875e73b0ef6f03fd56b881b3a9d8913d05d
whuang214 <whuang2146@gmail.com>
2024-04-24 16:24:25 -0400

    adds user model

---
+++ server/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  githubId: {
+    type: String,
+    required: true,
+    unique: true,
+  }, // the user's github id
+  username: String, // the user's github username (will be unique)
+  displayName: String, // the user's name
+  profileUrl: String, // url to the user's github profile
+  avatarUrl: String, // url to the user's github avatar
+  location: String, // the user's location
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
