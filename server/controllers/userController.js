831462684c462655732c62b5df06d4a1b5f6f0b7
whuang214 <whuang2146@gmail.com>
2024-04-24 23:27:49 -0400

    add search user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-// userController.js
+const User = require("../models/userModel");
 
 const getUser = (req, res) => {
   if (!req.user) {
@@ -10,6 +10,49 @@ const getUser = (req, res) => {
   });
 };
 
+const searchUser = async (req, res) => {
+  const { term } = req.query;
+  if (!term) {
+    return res.status(400).json({ error: "Search term is required." });
+  }
+
+  try {
+    // Search for users based on the search term using Mongoose
+    const results = await User.find({
+      $or: [
+        { username: { $regex: term, $options: "i" } }, // Case-insensitive username search
+        { displayName: { $regex: term, $options: "i" } }, // Case-insensitive display name search
+      ],
+    });
+    console.log("Search results:", results);
+    res.json({ results });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while searching for users." });
+  }
+};
+
+const getUserByID = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Find a user by ID using Mongoose
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving user by ID." });
+  }
+};
+
 module.exports = {
   getUser,
+  searchUser,
+  getUserByID,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.getUser);
+router.get("/search", userController.searchUser);
+router.get("/search:id", userController.getUserByID);
 
 module.exports = router;
