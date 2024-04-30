de421b017a8241c7d9615a30352b37d3375960f7
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2024-04-30 11:09:52 -0400

    maybe

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
       sameSite: 'none',
-      httpOnly: false,
+      httpOnly: true,
     },
   })
 );
