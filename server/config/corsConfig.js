27799948494c1735f422b0b5eb44659d7aa0d111
whuang214 <whuang2146@gmail.com>
2024-07-14 13:13:14 -0400

    cors settings

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -4,8 +4,8 @@ const allowedOrigins = [process.env.PROD_FRONTEND_ORIGIN];
 
 const corsOptions = {
   origin: function (origin, callback) {
+    // allow requests from development frontend
     if (process.env.NODE_ENV === "development") {
-      // In development, accept all origins for easier testing
       allowedOrigins.push(process.env.DEV_FRONTEND_ORIGIN);
     }
 
@@ -15,8 +15,8 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // If your app needs to handle credentials
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, // allows cookies to be send back
+  optionsSuccessStatus: 200, // success status for preflight requests
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
