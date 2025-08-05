7501ab1af359db11e4489ed0ad7d111eb37eba2f
Will Huang <41177162+whuang214@users.noreply.github.com>
2025-08-05 19:15:01 -0400

    add chat header group placeholder update

--- client/src/components/Chat/ChatWindow/ChatHeader.jsx
+++ client/src/components/Chat/ChatWindow/ChatHeader.jsx
@@ -1,8 +1,6 @@
 export const ChatHeader = ({ user, currentConversation }) => {
   if (!currentConversation) {
-    return <div>
-        Loading...
-    </div>;
+    return <div>Loading...</div>;
   }
 
   // make sure otherParticipant is updated when the currentConversation changes
@@ -10,10 +8,20 @@ export const ChatHeader = ({ user, currentConversation }) => {
   // if current convo is group than the group name is the title
   if (currentConversation.type === "group") {
     return (
-      <div className={"rounded-2xl shadow-lg text-white p-4 flex items-center" + " bg-" + `${user?.colors.mainColor}`}>
+      <div
+        className={
+          "rounded-2xl shadow-lg text-white p-4 flex items-center" +
+          " bg-" +
+          `${user?.colors.mainColor}`
+        }
+      >
         <img
-          className={"h-14 w-14 rounded-full border-2 mr-3" + " border-" + `${user?.colors.lightColor}`}
-          src={`https://via.placeholder.com/50`}
+          className={
+            "h-14 w-14 rounded-full border-2 mr-3" +
+            " border-" +
+            `${user?.colors.lightColor}`
+          }
+          src={`https://placehold.co/50`}
           alt="User Avatar"
         />
         <div>
@@ -28,9 +36,19 @@ export const ChatHeader = ({ user, currentConversation }) => {
     currentConversation.participants[0];
 
   return (
-    <div className={"rounded-2xl shadow-lg text-white p-4 flex items-center" + " bg-" + `${user?.colors.mainColor}`}>
+    <div
+      className={
+        "rounded-2xl shadow-lg text-white p-4 flex items-center" +
+        " bg-" +
+        `${user?.colors.mainColor}`
+      }
+    >
       <img
-        className={"h-14 w-14 rounded-full border-2 mr-3" + " border-" +`${user?.colors.lightColor}`}
+        className={
+          "h-14 w-14 rounded-full border-2 mr-3" +
+          " border-" +
+          `${user?.colors.lightColor}`
+        }
         src={`${otherParticipant.avatarUrl}`}
         alt="User Avatar"
       />
